a105673197c7c8db57b430f4e0e738b5a7092cf9
david millan <damillano93@gmail.com>
2021-05-26 21:29:05 -0500

    add test cases

---
+++ test_cases/isMutantFalse.js
@@ -0,0 +1,54 @@
+const testOK = module.exports
+
+testOK.x4 = {
+    "dna": [
+        "CTGC",
+        "CACC",
+        "CCGT",
+        "CGGG"
+    ]
+}
+testOK.x5 = {
+    "dna": [
+        "CTGCA",
+        "CACCC",
+        "CCGTA",
+        "CGGGA",
+        "ACAAG"
+    ]
+}
+testOK.x6 = {
+    "dna": [
+        "ATGCGC",
+        "CAGTGC",
+        "TTATGT",
+        "AGAAGG",
+        "CCACTA",
+        "TCACTG"
+    ]
+}
+
+testOK.x7 = {
+    "dna": [
+        "AATGCGC",
+        "CCAGTGC",
+        "CTTGTGT",
+        "TAGAAGG",
+        "GCCTCTA",
+        "ATCACTG",
+        "CTCACTG"
+    ]
+}
+
+testOK.x8 = {
+    "dna": [
+        "AATGCGCG",
+        "CCAGTGCG",
+        "TTTATGTA",
+        "TAGAAGGG",
+        "GCTCCTAG",
+        "ATCACTGA",
+        "CTCACTGG",
+        "GGTCACTC"
+    ]
+}
---
+++ test_cases/isMutantOk.js
@@ -0,0 +1,54 @@
+const testOK = module.exports
+
+testOK.x4 = {
+    "dna": [
+        "CTGC",
+        "CACC",
+        "CCGT",
+        "CGGG"
+    ]
+}
+testOK.x5 = {
+    "dna": [
+        "CTGCA",
+        "CACCA",
+        "CCGTA",
+        "CGGGA",
+        "AAAAG"
+    ]
+}
+testOK.x6 = {
+    "dna": [
+        "ATGCGC",
+        "CAGTGC",
+        "TTATGT",
+        "AGAAGG",
+        "CCCCTA",
+        "TCACTG"
+    ]
+}
+
+testOK.x7 = {
+    "dna": [
+        "AATGCGC",
+        "CCAGTGC",
+        "TTTATGT",
+        "TAGAAGG",
+        "GCCCCTA",
+        "ATCACTG",
+        "CTCACTG"
+    ]
+}
+
+testOK.x8 = {
+    "dna": [
+        "AATGCGCG",
+        "CCAGTGCG",
+        "TTTATGTG",
+        "TAGAAGGG",
+        "GCCCCTAG",
+        "ATCACTGG",
+        "CTCACTGG",
+        "GGTCACTC"
+    ]
+}
